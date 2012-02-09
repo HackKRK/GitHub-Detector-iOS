d5ce8badf34305a290e3d9d62b9dad32ca584f47
Błażej Biesiada <blazej@biesiada.net.pl>
2012-02-09 19:34:14 +0100

    First commit.

---
+++ GitHubDetector/HKAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  HKAppDelegate.h
+//  GitHubDetector
+//
+//  Created by Błażej Biesiada on 2/9/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface HKAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ GitHubDetector/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  GitHubDetector
+//
+//  Created by Błażej Biesiada on 2/9/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "HKAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([HKAppDelegate class]));
+    }
+}
