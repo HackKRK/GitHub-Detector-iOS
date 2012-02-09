12fab0903a9156dcd3f7415724b699e538f295de
test <test@felipe-imac.local>
2012-02-09 20:14:24 +0100

    cp

---
+++ GitHubDetector/MapViewController.h
@@ -0,0 +1,16 @@
+//
+//  MapViewController.h
+//  GitHubDetector
+//
+//  Created by test on 2/9/12.
+//  Copyright 2012 HackKrk. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+@interface MapViewController : UIViewController {
+    
+}
+
+@end
