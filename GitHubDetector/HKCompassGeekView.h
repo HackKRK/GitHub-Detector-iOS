c2820b7c9da90fb7b973d957aeed4550b33614f7
Krzysiek <kprofic@gmail.coma>
2012-02-09 20:15:10 +0100

    Added compass view label. Haha.

---
+++ GitHubDetector/HKCompassGeekView.h
@@ -0,0 +1,13 @@
+//
+//  HKCompassGeekView.h
+//  GitHubDetector
+//
+//  Created by Krzysztof Profic on 12-02-09.
+//  Copyright (c) 2012 HackKrk. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface HKCompassGeekView : UIView
+
+@end
