f3ef5b908144d00cbcbbcc1f893f7a979aae2fea
Karol S. Mazur <karol@applicake.com>
2012-02-09 23:38:26 +0100

    Implemented view that displays geeks name on the compass.

--- GitHubDetector/HKCompassGeekView.h
+++ GitHubDetector/HKCompassGeekView.h
@@ -2,12 +2,28 @@
 //  HKCompassGeekView.h
 //  GitHubDetector
 //
-//  Created by Krzysztof Profic on 12-02-09.
+//  Created by Karol S. Mazur on 09-02-2012.
 //  Copyright (c) 2012 HackKrk. All rights reserved.
 //
 
 #import <UIKit/UIKit.h>
+#import "HKGeek.h"
 
-@interface HKCompassGeekView : UIView
+// How does this class work?
+// You init it with the name of the geek. 
+// Then you give it a block that returns the distance (integer). 
+// When setNeedsDisplay calls redraw, we call our block for a new distance.
+
+typedef int (^DistanceToGeek)(void);
+
+@interface HKCompassGeekView : UIView {
+    UILabel *metersLabel;
+    UILabel *nameLabel;
+    DistanceToGeek getMeMyDistance;
+}
+
+- (id)initWithGeek:(HKGeek *)leGeek;
+
+@property (nonatomic, copy) DistanceToGeek getMeMyDistance;
 
 @end
--- GitHubDetector/HKCompassGeekView.m
+++ GitHubDetector/HKCompassGeekView.m
@@ -2,30 +2,67 @@
 //  HKCompassGeekView.m
 //  GitHubDetector
 //
-//  Created by Krzysztof Profic on 12-02-09.
+//  Created by Karol S. Mazur on 09-02-2012.
 //  Copyright (c) 2012 HackKrk. All rights reserved.
 //
 
+#import <QuartzCore/QuartzCore.h>
 #import "HKCompassGeekView.h"
+
+#pragma mark Private interface
+
+@interface HKCompassGeekView ()
+
+@end
+
+#pragma mark Implementation
 
 @implementation HKCompassGeekView
 
-- (id)initWithFrame:(CGRect)frame
-{
-    self = [super initWithFrame:frame];
-    if (self) {
-        // Initialization code
+@synthesize getMeMyDistance;
+
+- (id)initWithGeek:(HKGeek *)leGeek {
+    CGRect rect = CGRectMake(0, 0, 70, 15);
+    
+    if ((self = [super initWithFrame:rect])) {
+        // this should cause drawRect to not clear us
+        self.clearsContextBeforeDrawing = NO;
+        
+        // setup look of our view
+        self.backgroundColor = [UIColor colorWithRed:0.1f green:0.1f blue:0.1f alpha:0.5f];
+        self.opaque = NO;
+        self.layer.cornerRadius = 5.0;
+        
+        // setup the two label subviews
+        CGRect rectForMeters = CGRectMake(0, 0, 20, 15);
+        CGRect rectForName = CGRectMake(0, 0, 50, 15);
+        
+        metersLabel = [[UILabel alloc] initWithFrame:rectForMeters];
+        nameLabel = [[UILabel alloc] initWithFrame:rectForName];
+        
+        metersLabel.textColor = [UIColor whiteColor];
+        nameLabel.textColor = [UIColor whiteColor];
+        
+        metersLabel.font = [UIFont fontWithName:@"HelveticaNeue" size:12.0];
+        nameLabel.font = [UIFont fontWithName:@"HelveticaNeue" size:12.0];
+        
+        metersLabel.text = @"0 m";
+        nameLabel.text = leGeek.login;
+        
+        [self addSubview:metersLabel];
+        [self addSubview:nameLabel];
     }
     return self;
 }
 
-/*
-// Only override drawRect: if you perform custom drawing.
-// An empty implementation adversely affects performance during animation.
-- (void)drawRect:(CGRect)rect
-{
-    // Drawing code
+- (void)drawRect:(CGRect)rect {
+    // If the block exists then grab meters from it!
+    if (getMeMyDistance) {
+        int metersToGeek = getMeMyDistance();
+        metersLabel.text = [NSString stringWithFormat:@"%d m", metersToGeek];
+    }
+    
+    [super drawRect:rect];
 }
-*/
 
 @end
