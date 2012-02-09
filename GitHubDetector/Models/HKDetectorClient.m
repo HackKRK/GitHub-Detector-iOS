eac2d55c744f6c9df85830a287035847446972de
Błażej Biesiada <blazej@biesiada.net.pl>
2012-02-09 20:46:13 +0100

    Added last checkin method to Geek object.

---
+++ GitHubDetector/Models/HKDetectorClient.m
@@ -0,0 +1,13 @@
+//
+//  HKDetectorClient.m
+//  GitHubDetector
+//
+//  Created by Błażej Biesiada on 2/9/12.
+//  Copyright (c) 2012 HackKrk. All rights reserved.
+//
+
+#import "HKDetectorClient.h"
+
+@implementation HKDetectorClient
+
+@end
